a9c6d9b7b9f383c188cf4e4a7e1f38a20c62a682
Dominic <dominic.m.chu@gmail.com>
2019-08-15 04:39:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector('target');
+  return document.getElementById('nested').querySelector('target'f);
 }
 
 function increaseRankBy(n) {
